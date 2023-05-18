68b42958166ba90aaf4b2aabd08f16d0eaf47932
Dominik Mikołajczyk <minisiowa.skrzynka@gmail.com>
2023-05-18 17:11:11 +0200

    replacing lambdas with function references

--- src/pl/javastart/library/app/LibraryControl.java
+++ src/pl/javastart/library/app/LibraryControl.java
@@ -85,7 +85,7 @@ public class LibraryControl {
 
     private void printUsers() {
         printer.printUsers(library.getSortedUsers(
-                (u1, u2) -> u1.getLastName().compareToIgnoreCase(u2.getLastName())
+                Comparator.comparing(User::getFirstName, String.CASE_INSENSITIVE_ORDER)
         ));
     }
 
@@ -115,7 +115,8 @@ public class LibraryControl {
 
     private void printBooks() {
         printer.printBooks(library.getSortedPublications(
-                (p1, p2) -> p1.getTitle().compareToIgnoreCase(p2.getTitle())
+//                (p1, p2) -> p1.getTitle().compareToIgnoreCase(p2.getTitle())
+                Comparator.comparing(Publication::getTitle, String.CASE_INSENSITIVE_ORDER)
         ));
     }
 
@@ -145,7 +146,8 @@ public class LibraryControl {
 
     private void printMagazines() {
         printer.printMagazines(library.getSortedPublications(
-                (p1, p2) -> p1.getTitle().compareToIgnoreCase(p2.getTitle())
+//                (p1, p2) -> p1.getTitle().compareToIgnoreCase(p2.getTitle())
+                Comparator.comparing(Publication::getTitle, String.CASE_INSENSITIVE_ORDER)
         ));
     }
 
