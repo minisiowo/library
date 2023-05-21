0a52972d12976566e286ab6bfec27d3fe7312d8e
Dominik Mikołajczyk <minisiowa.skrzynka@gmail.com>
2023-05-21 13:07:23 +0200

    using streams to display collections

--- src/pl/javastart/library/io/ConsolePrinter.java
+++ src/pl/javastart/library/io/ConsolePrinter.java
@@ -1,44 +1,37 @@
 package pl.javastart.library.io;
 
-import pl.javastart.library.model.Book;
-import pl.javastart.library.model.LibraryUser;
-import pl.javastart.library.model.Magazine;
-import pl.javastart.library.model.Publication;
+import pl.javastart.library.model.*;
 
 import java.util.Collection;
 
 public class ConsolePrinter {
 
     public void printBooks(Collection<Publication> publications) {
-        int countBooks = 0;
-        for (Publication publication : publications) {
-            if (publication instanceof Book) {
-                printLine(publication.toString());
-                countBooks++;
-            }
-        }
+        long countBooks = publications.stream()
+                .filter(publication -> publication instanceof Book)
+                .peek(publication -> printLine(publication.toString()))
+                .count();
+
         if (countBooks == 0) {
             printLine("Brak książek w bibliotece.");
         }
     }
 
     public void printMagazines(Collection<Publication> publications) {
-        int countMagazines = 0;
-        for (Publication publication : publications) {
-            if (publication instanceof Magazine) {
-                printLine(publication.toString());
-                countMagazines++;
-            }
-        }
+        long countMagazines = publications.stream()
+                .filter(publication -> publication instanceof Magazine)
+                .peek(publication -> printLine(publication.toString()))
+                .count();
+
         if (countMagazines == 0) {
             printLine("Brak magazynów w bibliotece.");
         }
     }
 
     public void printUsers(Collection<LibraryUser> users) {
-        for (LibraryUser user : users) {
-            printLine(user.toString());
-        }
+        users.stream()
+                .map(User::toString)
+                .forEach(this::printLine);
     }
 
     public void printLine(String string) {
