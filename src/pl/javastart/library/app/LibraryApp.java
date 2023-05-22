372514e6690d5b573bf1c7933ad5e923522070ad
Dominik Mikołajczyk <minisiowa.skrzynka@gmail.com>
2023-05-22 18:25:44 +0200

    app version update

--- src/pl/javastart/library/app/LibraryApp.java
+++ src/pl/javastart/library/app/LibraryApp.java
@@ -3,7 +3,7 @@ package pl.javastart.library.app;
 import pl.javastart.library.io.ConsolePrinter;
 
 public class LibraryApp {
-    private static final String APP_NAME = "Biblioteka v1.8";
+    private static final String APP_NAME = "Biblioteka v2.0";
 
     public static void main(String[] args){
         System.out.println(APP_NAME);
